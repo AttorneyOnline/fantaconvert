ced628c778182b99cf4b376fbdd9dc74fb10a1ef
oldmud0 <oldmud0@users.noreply.github.com>
2018-02-16 12:18:25 -0600

    Rework tar procedure; case-insensitive emotions dir

--- convert.py
+++ convert.py
@@ -10,7 +10,7 @@ logger = logging.getLogger()
 
 def convert(char_dir, base_dir, temp_dir, target_dir):
     logger.info("-- Conversion started for {}".format(path.basename(char_dir)))
-    logger.info("Reading char.ini")
+    logger.debug("Reading char.ini")
     with open(path.join(char_dir, "char.ini")) as f:
         char_ini = ConfigParser()
         char_ini.read_string(f.read())
@@ -34,12 +34,12 @@ def convert(char_dir, base_dir, temp_dir, target_dir):
         pass
 
     # Copy all files to temp dir
-    logger.info("Copying original files")
+    logger.debug("Copying original files")
     temp_char_dir = path.join(temp_dir, "content")
     shutil.copytree(char_dir, temp_char_dir)
 
     # Copy extra files
-    logger.info("Copying blip sound effect")
+    logger.debug("Copying blip sound effect")
     try:
         blip_sfx = "sfx-blip" + char_ini["Options"]["gender"] + ".wav"
     except KeyError:
@@ -48,13 +48,18 @@ def convert(char_dir, base_dir, temp_dir, target_dir):
     shutil.copy2(path.join(base_dir, "sounds", "general", blip_sfx),
                  path.join(temp_char_dir, "blip.wav"))
 
-    logger.info("Converting emotes")
+    logger.debug("Converting emotes")
+    # Find case-insensitive emotions folder
+    emotions_folder = [d for d in os.listdir(
+        temp_char_dir) if d.lower() == "emotions"][0]
+
+    # Go through all emotes and find preanimations
     preanims = info["preanims"]
     for i in range(1, int(char_ini["Emotions"]["number"]) + 1):
         emote_raw = char_ini["Emotions"][str(i)].split("#")
         emote = {
             "name": emote_raw[0],
-            "icon": "emotions/button{}_on.png".format(i),
+            "icon": "{}/button{}_on.png".format(emotions_folder, i),
             "idle": "(a){}.gif".format(emote_raw[2]),
             "talking": "(b){}.gif".format(emote_raw[2]),
         }
@@ -91,41 +96,60 @@ def convert(char_dir, base_dir, temp_dir, target_dir):
                 preanims[preanim_name] = preanim
         info["emotes"].append(emote)
 
-    logger.info("Scanning files")
-    for root, dirs, files in os.walk(temp_char_dir):
-        for filename in files:
-            # Find some of the interjection WAV files.
-            # Surprisingly enough, some characters use
-            # capital letters in the filenames.
-            lowercase = filename.lower()
-            if lowercase == "holdit.wav":
-                info["objection_override"]["hold_it"] = filename
-            elif lowercase == "objection.wav":
-                info["objection_override"]["objection"] = filename
-            elif lowercase == "takethat.wav":
-                info["objection_override"]["take_that"] = filename
-            elif lowercase == "custom.wav":
-                info["objection_override"]["custom"] = filename
-            info["files"].append(filename)
+    logger.debug("Scanning files and creating content.tar")
+    with tarfile.open(path.join(temp_dir, "content.tar"), "w") as tar:
+        for root, dirs, files in os.walk(temp_char_dir):
+            for name in files:
+                filename = path.join(path.relpath(root, temp_char_dir), name)
+                filename.replace("\\", "/")
 
-    logger.info("Creating content.tar")
-    shutil.make_archive(path.join(temp_dir, "content"), "tar",
-                        root_dir=temp_char_dir, logger=logger)
+                # KILL THE DOT FOLDER WITH FIRE
+                if filename[:2] == "./":
+                    filename = filename[2:]
+
+                # Find some of the interjection WAV files.
+                # Surprisingly enough, some characters use
+                # capital letters in the filenames.
+                lowercase = filename.lower()
+                if lowercase == "holdit.wav":
+                    info["objection_override"]["hold_it"] = filename
+                elif lowercase == "objection.wav":
+                    info["objection_override"]["objection"] = filename
+                elif lowercase == "takethat.wav":
+                    info["objection_override"]["take_that"] = filename
+                elif lowercase == "custom.wav":
+                    info["objection_override"]["custom"] = filename
+                info["files"].append(filename)
+                logger.debug(f"{filename}")
+                full_path = path.join(temp_char_dir, filename)
+                i = tar.gettarinfo(full_path, arcname=filename)
+                i.uid = i.gid = 0
+                i.uname = i.gname = ""
+                if i.isreg():
+                    with open(full_path, "rb") as f:
+                        tar.addfile(i, fileobj=f)
+                else:
+                    tar.addfile(i)
+
+    #with tarfile.open(path.join(temp_dir, "content.tar"), "w") as tar:
+    #    tar.add(temp_char_dir, arcname=path.sep)
+    # shutil.make_archive(path.join(temp_dir, "content"), "tar",
+    #                     root_dir=temp_char_dir, logger=logger)
     
-    logger.info("Removing content folder")
+    logger.debug("Removing content folder")
     shutil.rmtree(temp_char_dir)
 
-    logger.info("Computing crc32 of content.tar")
     with open(path.join(temp_dir, "content.tar"), "rb") as f:
         hash = binascii.crc32(f.read())
         hash_str = "{:08x}".format(hash)
         info["hash"] = "crc32:{}".format(hash_str)
+    logger.info("CRC32: {}".format(hash_str))
 
-    logger.info("Writing info.json")
+    logger.debug("Writing info.json")
     with open(path.join(temp_dir, "info.json"), "w") as f:
         json.dump(info, f)
 
-    logger.info("Copying files")
+    logger.debug("Copying files")
     shutil.copytree(temp_dir, path.join(target_dir, hash_str))
 
     logger.info("-- Conversion complete!")
